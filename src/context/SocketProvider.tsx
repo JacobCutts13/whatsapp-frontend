3741d120d638425c184b9e59206b345ded7a8014
unknown <jacobdcutts@gmail.com>
2022-06-08 15:02:25 +0100

    connect to heroku

--- src/context/SocketProvider.tsx
+++ src/context/SocketProvider.tsx
@@ -2,29 +2,29 @@ import { createContext, useContext, useEffect, useState } from "react";
 import io, { Socket } from "socket.io-client";
 
 interface Props {
-  children: JSX.Element;
-  id: string;
+    children: JSX.Element;
+    id: string;
 }
 
 const SocketContext = createContext<Socket | null>(null);
 
 export function useSocket(): Socket | null {
-  return useContext(SocketContext);
+    return useContext(SocketContext);
 }
 
 export function SocketProvider(props: Props): JSX.Element {
-  const [socket, setSocket] = useState<Socket | null>(null);
+    const [socket, setSocket] = useState<Socket | null>(null);
 
-  useEffect((): (() => void) => {
-    const newSocket = io("http://localhost:5000", { query: { id: props.id } });
-    setSocket(newSocket);
+    useEffect((): (() => void) => {
+        const newSocket = io("https://whatsapp-jacob.herokuapp.com/", { query: { id: props.id } });
+        setSocket(newSocket);
 
-    return () => newSocket.close(); //close socket
-  }, [props.id]);
+        return () => newSocket.close(); //close socket
+    }, [props.id]);
 
-  return (
-    <SocketContext.Provider value={socket}>
-      {props.children}
-    </SocketContext.Provider>
-  );
+    return (
+        <SocketContext.Provider value={socket}>
+            {props.children}
+        </SocketContext.Provider>
+    );
 }
